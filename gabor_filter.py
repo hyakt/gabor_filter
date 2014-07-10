e65a497ed3e2be45f51f5f2be4da0d5be4415617
Hayato Kajiyama <kaji1216@gmail.com>
2014-07-10 22:22:28 +0900

    実行できました。余分なとこ削る

--- gabor_filter.py
+++ gabor_filter.py
@@ -1,7 +1,3 @@
-#
-# @module: And-Or Images
-# @copyright: Ken Kansky
-#
 import numpy as np
 from numpy import cos, sin, pi, log, exp, ceil, sqrt, abs
 from scipy.signal import convolve2d
@@ -11,16 +7,9 @@ import PIL.Image as Image
 
 class GaborFilter(object):
 
-    """
-      Generates a mask for the Gabor filter with the
-      given parameters. Defaults roughly match biological
-      filters observed in S1 cells in the visual cortex.
-
-      constrain : If True, the mask will have same dimension
-                  as the wavelength
-    """
-
     def __init__(self, wavelength, theta, gamma=2, a=0.5, constrain=False):
+
+        print("create instance")
 
         # Compute appropriate pixel dimensions for longest axis
         eta = -0.5 / (a * wavelength) ** 2
@@ -48,23 +37,24 @@ class GaborFilter(object):
         self.mask /= sqrt((self.mask * self.mask).sum())
 
     def convolve(self, image):
-        """
-        Convolves the input image with the stored Gabor mask.
-        The output image is the same dimension as the input, and values
-        beyond edges are assumed to have the same pixel value as the
-        nearest edge pixel.
-        """
         return abs(convolve2d(image, self.mask, mode='same'))
 
     def show(self):
-        """ Plots the wavelet. """
-        toimage(self.mask).show()
+        #        toimage(self.mask).show()
+        toimage(self.mask).save("./wave.png")
+        print("wave show call")
+
 
 if __name__ == '__main__':
-    gf = GaborFilter(10, 10)
+    gf = GaborFilter(10, 0)
 
-    pic = Image.open("./image.png")
-    pix = np.array(pic.getdata().reshape(pic.size[0], pic.size[1], 3))
+    pic = Image.open("./srcimg50.png")
+    pix = np.asarray(pic)
 
-    gf.convolve('./image.png')
+    convarr = gf.convolve(pix)
+    convimg = Image.fromarray(convarr)
+    convimg.convert('RGB').save('convimg.png')
+
+    print(convimg)
     gf.show()
+    print("end")
